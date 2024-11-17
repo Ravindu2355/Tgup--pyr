a742678e444d3d191bf4be2bbc556b59e6b20a57
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-17 22:20:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,56 @@ def up_f():
     url = request.args.get('url')
     cid = request.args.get('cid')
     opw = request.args.get('opw')
-    
+    try:
+      filename = url.split("/")[-1]  # Extract the filename from the URL
+      if '?' in filename:
+          filename = filename.split("?")[0]
+        
+      downloaded_size = 0  # Track the downloaded size
+      with open(filename, 'wb') as file:
+          for chunk in response.iter_content(chunk_size=1024):
+              if chunk:
+                  file.write(chunk)
+                  downloaded_size += len(chunk)
+                  # Update progress approximately every 2%
+                  if total_size > 0 and downloaded_size % (total_size // 50) == 0:
+                      #make a progress bar...
+                      progress_i = int(20 * downloaded_size / total_size)
+                      progress='[' + '✅️' * progress_i + '❌️' * (20 - progress_i) + ']'
+                      #progress = progress_bar(downloaded_size, total_size)
+                      percent = (downloaded_size / total_size) * 100
+                      #await reply_msg.edit_text(f"Downloading: {progress} {percent:.2f}%")
+                      print(percent)
+      thumb_path='thumb.jpg'
+      with VideoFileClip(filename) as video:
+              frame = video.get_frame(3.0)
+              img = Image.fromarray(frame)
+              img.save(thumb_path, "JPEG")
+     #await reply_msg.edit("Thumbnail generated. Uploading to Telegram...")
+      start_time=time.time()
+      await bot.send_video(
+          chat_id=cid,
+          video=filename,
+          caption=f'Uploaded: {filename}',
+          thumb=thumb_path,
+          supports_streaming=True,  # Ensure the video is streamable
+          progress=progress_for_pyrogram,
+          progress_args=(
+            "uploading!",
+             reply_msg,
+             start_time
+           )
+      )
+      try:
+         os.remove(filename)
+         if thumb_path and os.path.exists(thumb_path):
+           os.remove(thumb_path)
+      except Exception as e:
+         pass
+
+    except Exception as e:
+        print(f"Err while dl:{e}")
+        
 
 if __name__ == "__main__":
     app.run(port=8000)
