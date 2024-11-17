9bdc37510bf698748d6cb4b4d470013b83aa9ba9
Ravindu2355 <90367401+Ravindu2355@users.noreply.github.com>
2024-11-17 22:43:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 import math
 from PIL import Image
 
-opw = 2003
+opw = os.getenv('opw')
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,7 +16,7 @@ def hello_world():
 
 @app.route('/upload')
 def up_f():
-    required_params = ['url', 'cid', 'opw']
+    required_params = ['url', 'opw']
     missing_params = [param for param in required_params if not request.args.get(param)]
     if missing_params:
         return jsonify({
@@ -24,58 +24,15 @@ def up_f():
             'error': 'Some required parameters are missing or empty'
         }), 400
     url = request.args.get('url')
-    cid = request.args.get('cid')
-    opw = request.args.get('opw')
-    try:
-      filename = url.split("/")[-1]  # Extract the filename from the URL
-      if '?' in filename:
-          filename = filename.split("?")[0]
-        
-      downloaded_size = 0  # Track the downloaded size
-      with open(filename, 'wb') as file:
-          for chunk in response.iter_content(chunk_size=1024):
-              if chunk:
-                  file.write(chunk)
-                  downloaded_size += len(chunk)
-                  # Update progress approximately every 2%
-                  if total_size > 0 and downloaded_size % (total_size // 50) == 0:
-                      #make a progress bar...
-                      progress_i = int(20 * downloaded_size / total_size)
-                      progress='[' + '✅️' * progress_i + '❌️' * (20 - progress_i) + ']'
-                      #progress = progress_bar(downloaded_size, total_size)
-                      percent = (downloaded_size / total_size) * 100
-                      #await reply_msg.edit_text(f"Downloading: {progress} {percent:.2f}%")
-                      print(percent)
-      thumb_path='thumb.jpg'
-      with VideoFileClip(filename) as video:
-              frame = video.get_frame(3.0)
-              img = Image.fromarray(frame)
-              img.save(thumb_path, "JPEG")
-     #await reply_msg.edit("Thumbnail generated. Uploading to Telegram...")
-      start_time=time.time()
-      await bot.send_video(
-          chat_id=cid,
-          video=filename,
-          caption=f'Uploaded: {filename}',
-          thumb=thumb_path,
-          supports_streaming=True,  # Ensure the video is streamable
-          progress=progress_for_pyrogram,
-          progress_args=(
-            "uploading!",
-             reply_msg,
-             start_time
-           )
-      )
-      try:
-         os.remove(filename)
-         if thumb_path and os.path.exists(thumb_path):
-           os.remove(thumb_path)
-      except Exception as e:
-         pass
+    #cid = request.args.get('cid')
+    rpw = int(request.args.get('opw'))
+    if opw == rpw:
+        return await up_ul(url)
 
-    except Exception as e:
-        print(f"Err while dl:{e}")
-        
-
+    else:
+        return jsonify({
+            's':0,
+            'error': 'auth failed'
+        }), 400
 if __name__ == "__main__":
     app.run(port=8000)
